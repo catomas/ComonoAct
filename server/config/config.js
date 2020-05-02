4b1b97baea9833f05b61d19c30ab2107d5a4447e
Juan Camilo <juancami98.11@hotmail.com>
2020-05-01 22:31:47 -0500

    Despliegue en heroku

--- server/config/config.js
+++ server/config/config.js
@@ -23,9 +23,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ==========================
 let urlDB;
 
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/actitud';
+} else {
 
-
-    urlDB = 'mongodb+srv://catomas:trucha1998@cluster0-ximi4.mongodb.net/test?retryWrites=true&w=majority'
-
+    urlDB = 'mongodb+srv://catomas:trucha1998@cluster0-ximi4.mongodb.net/test?retryWrites=true&w=majority';
+}
 
 process.env.URLDB = urlDB;
